79a663fc1871fae3bf4e2fad5f95d075c77bfec3
jch0029987-glitch <jch0029987@gmail.com>
2026-09-05 11:27:12 -0500

    Add WebAppInterface class with requestToggleMouse method

--- app/src/main/java/com/example/messengerwrapper/WebAppInterface.kt
+++ app/src/main/java/com/example/messengerwrapper/WebAppInterface.kt
@@ -1,1 +1,11 @@
+package com.example.messengerwrapper
+
+import android.webkit.JavascriptInterface
+
+class WebAppInterface(private val onToggle: () -> Unit) {
+    @JavascriptInterface
+    fun requestToggleMouse() {
+        onToggle()
+    }
+}
 
