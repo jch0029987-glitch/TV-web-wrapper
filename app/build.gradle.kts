979b832cf08c3c64bcb0375cb85a4be7c24a0cfd
jch0029987-glitch <jch0029987@gmail.com>
2026-09-13 18:44:15 -0500

    Add GeckoView Engine dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+
+    // GeckoView Engine Dependency
+    implementation("org.mozilla.geckoview:geckoview:120.0.20231127143053")
 }
