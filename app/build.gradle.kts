c96a18774d1e331f92681a774d24acc4b7ed8d72
jch0029987-glitch <jch0029987@gmail.com>
2026-09-14 13:44:03 -0500

    Enable legacy packaging for JNI libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,12 @@ android {
         }
     }
 
+    packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
